82b93674fb586dc27ef41608c4c33ccbd4fa7086
mdubois <maxou.dub76@gmail.com>
2023-11-27 15:02:25 +0100

    is

---
+++ is.js
@@ -0,0 +1,50 @@
+
+is.num = function(value) {
+ return typeof value === 'number' && !isNaN(value)
+}
+
+is.nan = function(value) {
+ return typeof value === 'number' && isNaN(value)
+}
+
+is.str = function(value) {
+ return typeof value === 'string' || value instanceof String
+}
+
+is.bool = function(value) {
+ return typeof value === 'boolean'
+}
+
+is.undef = function(value) {
+ return typeof value === 'undefined'
+}
+
+is.def = function(value) {
+ return typeof value !== 'undefined'
+}
+
+is.arr = function(value) {
+ return Array.isArray(value)
+}
+
+is.obj = function(value) {
+ return typeof value === 'object' && value !== null && !is.arr(value)
+}
+
+is.fun = function(value) {
+ return typeof value === 'function'
+}
+
+is.truthy = function(value) {
+ return !!value
+}
+
+is.falsy = function(value) {
+ return !value
+}
+
+console.log(is.num(5))
+// output: true
+
+console.log(is.num('ciao'))
+// output: false
