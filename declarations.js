c9d54b885810e924a029bb89a6bf1834e7cd0f56
mdubois <maxou.dub76@gmail.com>
2023-11-27 10:01:51 +0100

    hello

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = "`, \, /, and '."
+const arr = [4,2]
+const obj = {
+    str : "Hello There",
+    num : 6,
+    bool : true,
+    undef : undefined,
+}
+const nested = {
+    arr : [4, undefined, 2],
+    obj: {
+        str : "Hello Romain",
+        num: 6,
+        bool: true,
+    }
+}
