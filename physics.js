60d55da57143d2224d3b1446aaea838a67dfdfc6
mdubois <maxou.dub76@gmail.com>
2023-11-27 16:22:19 +0100

    physics

---
+++ physics.js
@@ -0,0 +1,31 @@
+function getAcceleration(obj) {
+    const { f, m, Δv, Δt, t, d } = obj;
+   
+    if (f !== undefined && m !== undefined) {
+       const a = f / m;
+       return a;
+    }
+   
+    if (Δv !== undefined && Δt !== undefined) {
+       const a = Δv / Δt;
+       return a;
+    }
+   
+    if (t !== undefined && d !== undefined) {
+       const a = 2 * d / (t * t);
+       return a;
+    }
+   
+    return "impossible";
+   }
+
+   const acceleration = getAcceleration({
+    f: 10,
+    m: 5,
+     Δv: 100,
+     Δt: 50,
+    t: 1,
+    d: 10
+   });
+   
+   console.log(acceleration); // Output: 18s
