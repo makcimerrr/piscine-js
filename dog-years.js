f45cd7589a65b1e4e3ef757f09aaed8db9556b3c
mdubois <maxou.dub76@gmail.com>
2023-11-27 12:41:29 +0100

    dogyears

---
+++ dog-years.js
@@ -0,0 +1,21 @@
+const earthYearInSeconds = 31557600;
+const dogYearsPerHumanYear = 7;
+
+const orbitalPeriods = {
+ mercury: 0.2408467,
+ venus: 0.61519726,
+ mars: 1.8808158,
+ jupiter: 11.862615,
+ saturn: 29.447498,
+ uranus: 84.016846,
+ neptune: 164.79132,
+};
+
+function dogYears(planet, ageInSeconds) {
+ const ageInEarthYears = ageInSeconds / earthYearInSeconds;
+ const ageInDogYears = ageInEarthYears * dogYearsPerHumanYear;
+ const orbitalPeriod = orbitalPeriods[planet];
+
+ const ageOnPlanetInYears = ageInDogYears / orbitalPeriod;
+ return Math.round(ageOnPlanetInYears * 100) / 100;
+}
