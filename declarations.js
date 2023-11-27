f5c46e17dcfbe7057b4581ba02d66b831b284c31
mdubois <maxou.dub76@gmail.com>
2023-11-27 10:28:20 +0100

    up

--- declarations.js
+++ declarations.js
@@ -1,44 +1,34 @@
-// Define escapeStr constant
-const escapeStr = "`, \\, /, \" and \'";
+const escapeStr = "`, \\, /, '.\"";
 
-// Define arr constant
 const arr = [4, '2'];
 
-// Define obj constant
 const obj = Object.freeze({
-  str: 'someString',
-  num: 42,
-  bool: true,
-  undef: undefined,
-  nested: Object.freeze({
-    arr: Object.freeze([4, undefined, '2']),
-    obj: Object.freeze({
-      str: 'nestedString',
-      num: 3.14,
-      bool: false
-    })
-  })
+    str: "Hello There",
+    num: 6,
+    bool: true,
+    undef: undefined,
 });
 
-// Freeze arr, nested, and obj
-Object.freeze(arr);
-Object.freeze(obj.nested);
-Object.freeze(obj.nested.arr);
-Object.freeze(obj.nested.obj);
+const nested = Object.freeze({
+    arr: Object.freeze([4, undefined, '2']),
+    obj: Object.freeze({
+        str: "Hello",
+        num: 6,
+        bool: true,
+    }),
+});
 
 // Testing if the constants are truly constant
 // Uncommenting the next line should result in an error
 // arr.push(5);
 
 // Uncommenting the next line should result in an error
-// obj.nested.str = 'newString';
+// obj.num = 7;
 
 // Uncommenting the next line should result in an error
-// obj.nested.arr.push(5);
-
-// Uncommenting the next line should result in an error
-// obj.nested.obj.num = 42.5;
+// nested.obj.str = "Hi";
 
 console.log(escapeStr);
 console.log(arr);
 console.log(obj);
+console.log(nested);
