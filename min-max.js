8f2ee8801bd6aa2f30cba8495eede5d55e9b15fd
mdubois <maxou.dub76@gmail.com>
2023-11-27 14:28:04 +0100

    minmax

--- min-max.js
+++ min-max.js
@@ -1,9 +1,21 @@
 function max(number1, number2){
-    return Math.max(number1, number2)
+    const bool = number1>number2
+
+    if (bool === true){
+        return number1
+    }else{
+        return number2
+    }
 }
 
 function min(number1, number2){
-    return Math.min(number1, number2)
+    const bool = number1>number2
+
+    if (bool === true){
+        return number2
+    }else{
+        return number1
+    }
 }
 console.log(max(4,5))
 console.log(min(4,5))
