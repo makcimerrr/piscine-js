365efdf8507d8ecba5b6d07e23caf91d4e13b569
mdubois <maxou.dub76@gmail.com>
2023-11-27 11:12:52 +0100

    up

--- mutability.js
+++ mutability.js
@@ -1,13 +1,14 @@
-const clone1 = Object.freeze ({
-    name: Object.freeze('Rick'),
-    age: Object.freeze(78),
-    country: Object.freeze('FR'),
-  });
-
-  const clone2 = Object.freeze ({
-    name: Object.freeze('Rick'),
-    age: Object.freeze(78),
-    country: Object.freeze('FR'),
-  });
-
-  const samePerson = { ...person};
+ // Create deep copies of person
+   const clone1 = JSON.parse(JSON.stringify(person));
+   const clone2 = JSON.parse(JSON.stringify(person));
+   
+   // Create a reference to the same object
+   const samePerson = person;
+   
+   // Modify the original person object
+   person.age += 1;
+   person.country = 'FR';
+   
+   console.log(clone1); // Output: { name: 'Rick', age: 77, country: 'US' }
+   console.log(clone2); // Output: { name: 'Rick', age: 77, country: 'US' }
+   console.log(samePerson); // Output: { name: 'Rick', age: 78, country: 'FR' }
