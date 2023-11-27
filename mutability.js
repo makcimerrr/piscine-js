e153a5424b2cd2252b4ac0e68faa4fb24e5c721b
mdubois <maxou.dub76@gmail.com>
2023-11-27 11:01:00 +0100

    up

---
+++ mutability.js
@@ -0,0 +1,17 @@
+const clone1 = Object.freeze ({
+    name: Object.freeze('Rick'),
+    age: Object.freeze(78),
+    country: Object.freeze('FR'),
+  });
+
+  const clone2 = Object.freeze ({
+    name: Object.freeze('Rick'),
+    age: Object.freeze(78),
+    country: Object.freeze('FR'),
+  });
+
+  const samePerson = {
+    name: 'Rick',
+    age: 78,
+    country: 'FR',
+  }
