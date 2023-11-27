43ead4a39e2f81aedcd9d9e28c2c5ce3eaaf2a54
mdubois <maxou.dub76@gmail.com>
2023-11-27 12:03:23 +0100

    kiss

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,14 +1,12 @@
-// Define the first function
+
 function first(str) {
     return str[0];
 }
 
-// Define the last function
 function last(str) {
-    return str.charAt(str.length - 1);
+    return str[str.length - 1];
 }
 
-// Define the kiss function
 function kiss(str) {
     return [first(str), last(str)];
 }
