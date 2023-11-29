bf270e95fc3d36d372db3c6e3640dff0436e5864
mdubois <maxou.dub76@gmail.com>
2023-11-29 14:44:21 +0100

    index

--- index-of.js
+++ index-of.js
@@ -33,7 +33,7 @@ function lastIndexOf(arr, value,index){
         let i = arr.length
         while(i > 0){
             if (arr[i] === value){
-                return i - index
+                return i
             }
             i--
         }
@@ -67,7 +67,7 @@ function includes(arr,value){
 
 
 console.log(includes([7, 8, 9, 5, 6, 4], 4))
-console.log(lastIndexOf([7, 8, 9, 5, 8, 8, 8],8, 0))
+console.log(lastIndexOf([0, 0, 't', 't'], 't', 3))
 console.log(indexOf(['t', 0, 0, 't'], 't', 1))
 console.log(indexOf([7, 8, 9, 5, 6, 4],4))
 console.log(indexOf([7, 8, 9, 5, 6, 4],3))
