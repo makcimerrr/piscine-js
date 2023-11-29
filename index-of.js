904536e554c6a5d062c1ec1d737858be1ce5b743
mdubois <maxou.dub76@gmail.com>
2023-11-29 12:15:50 +0100

    index

--- index-of.js
+++ index-of.js
@@ -4,8 +4,14 @@ function indexOf(arr, value){
     if (value >= arr.length){
         return -1
     }
-    
+
+    let i = 0
+
     if (value !== undefined){
+        while(i < arr.length){
+            arr[value]
+            i++
+        }
         return arr[value]
     }else{
         return arr[0]
@@ -17,7 +23,13 @@ function lastIndexOf(arr, value){
         return -1
     }
 
+    let i = 0
+
     if (value !== undefined){
+        while(i > arr.length){
+            arr[value]
+            i--
+        }
         return arr[arr.length - value]
     }else{
         return arr[arr.length - 1]
@@ -31,3 +43,5 @@ function includes(arr,value){
         return true
     }
 }
+
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1],2))
