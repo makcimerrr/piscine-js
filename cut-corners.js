d417b52e997c0d8b64f32df8da3f6cbfff6db4ab
MaXDuB76 <dbmx76@gmail.com>
2023-11-30 09:42:00 +0100

    qsmkjkdf

--- cut-corners.js
+++ cut-corners.js
@@ -1,27 +1,31 @@
 function customRound(num) {
-    const decimal = num - parseInt(num);
-    return decimal >= 0.5 ? parseInt(num) + 1 : parseInt(num);
+    const decimal = num - (num | 0);
+    return decimal >= 0.5 ? (num | 0) + 1 : num | 0;
   }
   
   function customCeil(num) {
-    return num === parseInt(num) ? num : (num > 0 ? parseInt(num) + 1 : parseInt(num));
+    return num === (num | 0) ? num : (num > 0 ? (num | 0) + 1 : num | 0);
   }
   
   function customFloor(num) {
-    return parseInt(num);
+    return num | 0;
   }
   
   function customTrunc(num) {
-    return num >= 0 ? parseInt(num) : parseInt(num) - 1;
+    return num >= 0 ? num | 0 : -(Math.abs(num) | 0);
   }
   
-  // Testing the functions
-  console.log(customRound(4.49));  // Output: 4
-  console.log(customRound(4.5));   // Output: 5
-  console.log(customCeil(4.01));   // Output: 5
-  console.log(customCeil(4.99));   // Output: 5
-  console.log(customFloor(4.9));   // Output: 4
-  console.log(customFloor(4.01));  // Output: 4
-  console.log(customTrunc(4.9));   // Output: 4
-  console.log(customTrunc(-4.1));  // Output: -4
+// Testing the functions
+console.log(customRound(4.49));  // Output: 4
+console.log(customRound(4.5));   // Output: 5
+console.log(customCeil(4.01));   // Output: 5
+console.log(customCeil(4.99));   // Output: 5
+console.log(customFloor(4.9));   // Output: 4
+console.log(customFloor(4.01));  // Output: 4
+console.log(customTrunc(4.9));   // Output: 4
+console.log(customTrunc(-4.1));  // Output: -4
+  
+  
+  
+
   
