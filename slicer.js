654602f1ceea20360943c67965ee6361109d5cdf
MaXDuB76 <dbmx76@gmail.com>
2023-11-30 07:42:32 +0100

    slicer

--- slicer.js
+++ slicer.js
@@ -4,18 +4,33 @@ function slice(arr, startIndex, opt){
         if (startIndex === undefined && opt === undefined){
             return arr
         }else if (startIndex !== undefined && opt === undefined) {
-            for ( let i= startIndex; i < arr.length; i++){
-                result = result + arr[i]
+            if (startIndex > 0){
+                for ( let i= startIndex; i < arr.length; i++){
+                    result = result + arr[i]
+                }
+                return result
+            } else {
+                for ( let i= arr.length + startIndex; i < arr.length; i++){
+                    result = result + arr[i]
+                }
+                return result
             }
-            return result
         }else if (startIndex !== undefined && opt !== undefined) {
-            for ( let i= startIndex; i < opt; i++){
-                result = result + arr[i]
+            if (startIndex > 0 && opt > 0){
+                for ( let i= startIndex; i < opt; i++){
+                    result = result + arr[i]
+                }
+                return result
+            } else if (startIndex > 0 && opt < 0){
+                for ( let i= startIndex; i < arr.length + opt; i++){
+                    result = result + arr[i]
+                }
+                return result
             }
-            return result
         }
     }
 }
 
 
-console.log(slice('abcdef', 2))
+console.log(slice(['ant', 'bison', 'camel', 'duck', 'elephant'], 2, -1))
+console.log(slice('abcdef', 2, 3))
