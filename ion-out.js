ce381465a478f1e6086a4cb434aa124931604f1b
mdubois <maxou.dub76@gmail.com>
2023-12-01 12:20:23 +0100

    likjgh

--- ion-out.js
+++ ion-out.js
@@ -1,20 +1,17 @@
-function ionOut(inputString) {
-    const words = inputString.split(/\s+/); // Split the input string into words
+function ionOut(str) {
+    const words = str.split(/\s+/); // Split the input string into words
     const result = [];
   
     for (const word of words) {
-      const match = /ion[^,]*,?/i.exec(word);
-  
-      if (match && match[0].length > 0) {
+      if (/t.*ion[^,]*,?/i.test(word)) {
         // Remove 'ion' part and add the modified word to the result array
-        const modifiedWord = match[0].replace(/ion[^,]*,?/i, );
+        const modifiedWord = word.replace(/ion[^,]*,?/i, '');
         result.push(modifiedWord);
       }
     }
   
     return result;
   }
-  
-  
 
-console.log(ionOut('transfusion'));
+console.log(ionOut('attention, direction'));
+console.log(ionOut('transfu'));
