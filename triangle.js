eb9807cdc2e3a11af0a969af7d489c610f23fa73
mdubois <maxou.dub76@gmail.com>
2023-12-01 18:59:48 +0100

    iouyht

--- triangle.js
+++ triangle.js
@@ -2,12 +2,11 @@ function triangle(char, height) {
   let result = "";
 
   for (let i = 1; i <= height; i++) {
-    result += " ".repeat(height - i) + char.repeat(2 * i - 1) + "\n";
+    result += char.repeat(i) + "\n";
   }
 
-  return result;
+  return result.trim();
 }
 
 // Example usage:
-const triangleString = triangle("*", 5);
-console.log(triangleString);
+console.log(triangle("#", 4));
