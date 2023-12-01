4358839fa41f0f3f3fc7bfec7467475b10270c34
mdubois <maxou.dub76@gmail.com>
2023-12-01 11:54:32 +0100

    likjgh

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,10 @@
-function sameAmount(inputString, regex1, regex2) {
-    const matchCount1 = (inputString.match(regex1));
-    const matchCount2 = (inputString.match(regex2));
+function sameAmount(str, regex1, regex2) {
+  rexp1 = new RegExp(regex1, "g");
+    rexp2 = new RegExp(regex2, "g");
+    let matchCount1 = str.match(regex1);
+    let matchCount2 = str.match(regex2);
   
     return matchCount1 !== null && matchCount2 !== null && matchCount1.length === matchCount2.length;
   }
 
-  console.log(sameAmount('data', /q /, /qqqqqqq/))
+  console.log(!sameAmount('data', /q /, /qqqqqqq/))
