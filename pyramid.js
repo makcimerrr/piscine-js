0df40d08f738bb76b4adb7035d2ebbd2eab2385f
mdubois <maxou.dub76@gmail.com>
2023-12-01 22:25:13 +0100

    ds*

--- pyramid.js
+++ pyramid.js
@@ -2,10 +2,13 @@ function pyramid(char, height) {
   let result = "";
 
   for (let i = 1; i <= height; i++) {
-    result += " ".repeat(height - i) + char.repeat(2 * i - 1) + "\n";
+    result += " ".repeat(height - i) + char.repeat(2 * i - 1);
+    if (i < height) {
+      result += "\n";
+    }
   }
 
-  return result.trimEnd();
+  return result;
 }
 
 console.log(pyramid("#", 5));
