8d6958b9b7771d820a452ab2204e1125b16764a9
mdubois <maxou.dub76@gmail.com>
2023-12-01 11:04:49 +0100

    ikjkh

--- get.js
+++ get.js
@@ -1,19 +1,8 @@
 function get(src, path) {
-    const a = path.split('.');
-  
-    for (let i = 0; i < a.length; i++) {
-      const key = a[i];
-  
-      if (src && typeof src === 'object' && key in src) {
-        src = src[key];
-      } else {
-        return undefined;
-      }
-    }
-  
-    return src;
-  }
-  
-const src = { nested: { key: 'peekaboo' } }
-const path = 'nested.key'
-console.log(get(src, path)) // -> 'peekaboo'
+    return path.split(".").reduce(function (obj, key) {
+        if (obj === undefined) {
+            return undefined;
+        }
+        return obj[key];
+    }, src);
+}
