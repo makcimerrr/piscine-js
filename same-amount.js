24a10beff564fca91089179246885f326ce83463
mdubois <maxou.dub76@gmail.com>
2023-12-01 11:51:20 +0100

    likjgh

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,8 @@
 function sameAmount(inputString, regex1, regex2) {
-    const matchCount1 = (inputString.match(regex1) || []).length;
-    const matchCount2 = (inputString.match(regex2) || []).length;
+    const matchCount1 = (inputString.match(regex1));
+    const matchCount2 = (inputString.match(regex2));
   
-    return matchCount1 === matchCount2;
+    return matchCount1 !== null && matchCount2 !== null && matchCount1.length === matchCount2.length;
   }
+
+  console.log(sameAmount('data', /q /, /qqqqqqq/))
