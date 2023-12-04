6d281fe779960e15359a8f929882ca0cd665cfcf
MaXDuB76 <dbmx76@gmail.com>
2023-12-04 13:17:25 +0100

    likjhg

--- get-some-time.js
+++ get-some-time.js
@@ -1,35 +1,27 @@
 function firstDayWeek(weekNumber, year) {
-  // Convert input year to a number
-  year = Number(year);
-
-  // Check if the year is less than four digits
-  if (isNaN(year) || year < 1000 || year > 9999) {
-    return "Invalid year";
+  let firstDayOfYear = new Date(year, 0, 1);
+  let goodDate = firstDayOfYear;
+  const firstDay = firstDayOfYear.getDay();
+  const testFirstDay = (8 - firstDay) % 7;
+  const firstMondayDate =
+    firstDayOfYear.getTime() + testFirstDay * 24 * 60 * 60 * 1000;
+  if (testFirstDay == 0) {
+    weekNumber++;
+  }
+  const firstDayOfWeekSelected =
+    firstMondayDate + (weekNumber - 2) * 7 * 24 * 60 * 60 * 1000;
+  const calculatedDate = new Date(firstDayOfWeekSelected);
+  if (calculatedDate > firstDayOfYear) {
+    goodDate = calculatedDate;
   }
 
-  // Create a new Date object for January 1st of the specified year
-  let firstDayOfYear = new Date(year, 0, 1);
-
-  // Calculate the day of the week for January 1st
-  let dayOfWeek = firstDayOfYear.getDay();
-
-  // Calculate the offset to the first Monday of the week
-  let offset = (dayOfWeek - 2 + 7) % 7;
-
-  // Calculate the date of the first Monday of the week
-  let firstMonday = new Date(firstDayOfYear);
-  firstMonday.setDate(
-    firstDayOfYear.getDate() + (1 - offset) + (weekNumber - 1) * 7
-  );
-
   // Format the date as dd-mm-yyyy
-  let dd = String(firstMonday.getDate()).padStart(2, "0");
-  let mm = String(firstMonday.getMonth() + 1).padStart(2, "0");
-  let yyyy = firstMonday.getFullYear();
+  let dd = String(goodDate.getDate()).padStart(2, "0");
+  let mm = String(goodDate.getMonth() + 1).padStart(2, "0");
+  let yyyy = goodDate.getFullYear();
 
   return `${dd}-${mm}-${yyyy}`;
 }
-
 // Example usage:
 console.log(firstDayWeek(1, 1000)); // Output: "01-01-1000"
-console.log(firstDayWeek(52, 1000)); // Output: "23-12-1000"
+console.log(firstDayWeek(52, 1000)); // Output: "22-12-1000"
