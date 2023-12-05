7804712373c6635890215b10710b5621264e608f
MaXDuB76 <dbmx76@gmail.com>
2023-12-05 14:35:45 +0100

    lkjhg

--- currify.js
+++ currify.js
@@ -5,10 +5,3 @@ function currify(funcs) {
     }, args)[0];
   };
 }
-const mult2 = (el1, el2) => el1 * el2;
-console.log(mult2(2, 2)); // result expected 4
-
-const mult2Curried = currify(mult2);
-
-console.log(mult2Curried(2)(2)); // result expected 4
-// (same result, with a function that has technically only one argument)
