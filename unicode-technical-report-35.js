c5c451f1da23077554b8c986a3be15f65bdc9bcb
MaXDuB76 <dbmx76@gmail.com>
2023-12-05 15:13:50 +0100

    lmjkhg

--- unicode-technical-report-35.js
+++ unicode-technical-report-35.js
@@ -16,29 +16,47 @@ function format(date, formatString) {
 
   const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  const padZero = (num) => (num < 10 ? "0" : "") + num;
+  const padZero = (num, length) => {
+    const numString = String(num);
+    return "0".repeat(Math.max(0, length - numString.length)) + numString;
+  };
+
+  function getFullYearString(year) {
+    const absYear = Math.abs(year);
+    if (absYear > 0 && absYear < 10) {
+      return "000" + absYear;
+    } else if (absYear > 9 && absYear < 100) {
+      return "00" + absYear;
+    } else if (absYear > 99 && absYear < 1000) {
+      return "0" + absYear;
+    }
+    return String(absYear);
+  }
 
   const formatOptions = {
-    y: Math.abs(date.getFullYear()),
-    yyyy: date.getFullYear(),
+    y:
+      date.getFullYear() < 0
+        ? Math.abs(date.getFullYear())
+        : date.getFullYear(),
+    yyyy: getFullYearString(date.getFullYear()),
     G: "AD",
     GGGG: "Anno Domini",
     M: date.getMonth() + 1,
-    MM: padZero(date.getMonth() + 1),
+    MM: padZero(date.getMonth() + 1, 2),
     MMM: months[date.getMonth()],
     MMMM: months[date.getMonth()],
     d: date.getDate(),
-    dd: padZero(date.getDate()),
+    dd: padZero(date.getDate(), 2),
     E: daysOfWeek[date.getDay()],
     EEEE: daysOfWeek[date.getDay()],
     h: date.getHours() % 12 || 12,
-    hh: padZero(date.getHours() % 12 || 12),
+    hh: padZero(date.getHours() % 12 || 12, 2),
     m: date.getMinutes(),
-    mm: padZero(date.getMinutes()),
+    mm: padZero(date.getMinutes(), 2),
     s: date.getSeconds(),
-    ss: padZero(date.getSeconds()),
+    ss: padZero(date.getSeconds(), 2),
     H: date.getHours(),
-    HH: padZero(date.getHours()),
+    HH: padZero(date.getHours(), 2),
     a: date.getHours() < 12 ? "AM" : "PM",
   };
 
@@ -46,10 +64,12 @@ function format(date, formatString) {
 
   return formatString.replace(regex, (match) => formatOptions[match]);
 }
-/*const d = new Date("7 January 1985, 3:08:19");
 
+//EX
+const d = new Date("7 January 1985, 3:08:19");
 console.log(format(d, "HH(mm)ss [dd] <MMM>")); // -> '03(08)19 [07] <Jan>'
 console.log(format(d, "M"));
 
 const eclipse = new Date(-585, 4, 28);
-console.log(format(eclipse, "y"));*/
+console.log(format(eclipse, "yyyy"));
+console.log(format(eclipse, "y"));
