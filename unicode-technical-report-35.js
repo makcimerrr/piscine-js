cc9090b06fc0f1ca3ab6a540e6904936955f859a
MaXDuB76 <dbmx76@gmail.com>
2023-12-05 14:49:17 +0100

    k,jhvg

--- unicode-technical-report-35.js
+++ unicode-technical-report-35.js
@@ -1,85 +1,5 @@
-function format(date, formatStr) {
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  const milliseconds = date.getMilliseconds();
-  const dayOfWeek = date.getDay();
-
-  let formattedDate = "";
-  let currentChar;
-  let nextChar;
-  let count;
-
-  for (let i = 0; i < formatStr.length; i++) {
-    currentChar = formatStr[i];
-    nextChar = formatStr[i + 1];
-    count = 1;
-
-    while (currentChar === nextChar && i < formatStr.length - 1) {
-      count++;
-      i++;
-      nextChar = formatStr[i + 1];
-    }
-
-    switch (currentChar) {
-      case "y":
-        if (count === 2) {
-          formattedDate += (year % 100).toString().padStart(2, "0");
-        } else {
-          formattedDate += year.toString();
-        }
-        break;
-      case "M":
-        if (count === 2) {
-          formattedDate += month.toString().padStart(2, "0");
-        } else if (count === 3) {
-          formattedDate += getMonthShortName(month);
-        } else {
-          formattedDate += getMonthFullName(month);
-        }
-        break;
-      case "d":
-        formattedDate += day.toString().padStart(2, "0");
-        break;
-      case "E":
-        if (count === 3) {
-          formattedDate += getDayShortName(dayOfWeek);
-        } else {
-          formattedDate += getDayFullName(dayOfWeek);
-        }
-        break;
-      case "h":
-        formattedDate += (hours % 12 || 12).toString().padStart(2, "0");
-        break;
-      case "H":
-        formattedDate += hours.toString().padStart(2, "0");
-        break;
-      case "m":
-        formattedDate += minutes.toString().padStart(2, "0");
-        break;
-      case "s":
-        formattedDate += seconds.toString().padStart(2, "0");
-        break;
-      case "S":
-        formattedDate += milliseconds.toString().padStart(3, "0");
-        break;
-      case "a":
-        formattedDate += hours < 12 ? "AM" : "PM";
-        break;
-      default:
-        formattedDate += currentChar;
-        break;
-    }
-  }
-
-  return formattedDate;
-}
-
-function getMonthShortName(month) {
-  const monthNames = [
+function format(date, formatString) {
+  const months = [
     "Jan",
     "Feb",
     "Mar",
@@ -93,45 +13,36 @@ function getMonthShortName(month) {
     "Nov",
     "Dec",
   ];
-  return monthNames[month - 1];
+
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const padZero = (num) => (num < 10 ? "0" : "") + num;
+
+  const formatOptions = {
+    y: date.getFullYear(),
+    yyyy: date.getFullYear(),
+    G: "AD",
+    GGGG: "Anno Domini",
+    M: date.getMonth() + 1,
+    MM: padZero(date.getMonth() + 1),
+    MMM: months[date.getMonth()],
+    MMMM: months[date.getMonth()],
+    d: date.getDate(),
+    dd: padZero(date.getDate()),
+    E: daysOfWeek[date.getDay()],
+    EEEE: daysOfWeek[date.getDay()],
+    h: date.getHours() % 12 || 12,
+    hh: padZero(date.getHours() % 12 || 12),
+    m: date.getMinutes(),
+    mm: padZero(date.getMinutes()),
+    s: date.getSeconds(),
+    ss: padZero(date.getSeconds()),
+    H: date.getHours(),
+    HH: padZero(date.getHours()),
+    a: date.getHours() < 12 ? "AM" : "PM",
+  };
+
+  const regex = /y+|G+|M+|d+|E+|h+|m+|s+|H+|a+/g;
+
+  return formatString.replace(regex, (match) => formatOptions[match]);
 }
-
-function getMonthFullName(month) {
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  return monthNames[month - 1];
-}
-
-function getDayShortName(dayOfWeek) {
-  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return dayNames[dayOfWeek];
-}
-
-function getDayFullName(dayOfWeek) {
-  const dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  return dayNames[dayOfWeek];
-}
-
-const d = new Date("7 January 1985, 3:08:19");
-
-console.log(format(d, "HH(mm)ss [dd] <MMM>")); // -> '03(08)19 [07] <Jan>'
