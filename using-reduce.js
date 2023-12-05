a63c7683cfc1bd9870ace3b3219b4041206a92b8
MaXDuB76 <dbmx76@gmail.com>
2023-12-05 11:01:18 +0100

    kljh

---
+++ using-reduce.js
@@ -0,0 +1,29 @@
+function adder(array, opt) {
+  if (opt === undefined) {
+    opt = 0;
+  }
+  return array.reduce((accumulator, number) => accumulator + number, opt);
+}
+
+const array1 = [1, 2, 3, 4];
+console.log(adder(array1));
+
+function sumOrMul(array, opt) {
+  if (opt === undefined) {
+    opt = 0;
+  }
+  return array.reduce((accumulator, number) => {
+    return number % 2 === 0 ? accumulator * number : accumulator + number;
+  }, opt);
+}
+
+const numbersArray = [1, 2, 3, 5, 8];
+
+console.log(sumOrMul(numbersArray, 5));
+
+function funcExec(array, opt) {
+  if (opt === undefined) {
+    opt = 0;
+  }
+  return array.reduce((result, func) => func(result), opt);
+}
