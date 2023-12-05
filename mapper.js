265c1fd57e029b83de15914aa2a3ccf0611e273b
MaXDuB76 <dbmx76@gmail.com>
2023-12-05 11:54:43 +0100

    kjlhjgh

---
+++ mapper.js
@@ -0,0 +1,17 @@
+function map(array, callback) {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const result = callback(array[i], i, array);
+    resultArray.push(result);
+  }
+  return resultArray;
+}
+
+function flatMap(array, callback) {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const result = callback(array[i], i, array);
+    resultArray.push(result);
+  }
+  return resultArray;
+}
