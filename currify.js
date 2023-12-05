9a4b7c3bc3f4aeabca6d7335b34e61b7eafdb8fa
MaXDuB76 <dbmx76@gmail.com>
2023-12-05 14:40:35 +0100

    lkjhg

--- currify.js
+++ currify.js
@@ -1,7 +1,11 @@
 function currify(funcs) {
-  return function (...args) {
-    return funcs.reduce(function (result, func) {
-      return [func(...result)];
-    }, args)[0];
+  return function curried(...args) {
+    if (args.length >= funcs.length) {
+      return funcs.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
   };
 }
