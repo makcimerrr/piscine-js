afb5a2371cfa3d5a83540b88246c2a8f75c0bd9d
MaXDuB76 <dbmx76@gmail.com>
2023-12-05 12:35:40 +0100

    jlghf

---
+++ reduce.js
@@ -0,0 +1,39 @@
+function fold(array, callback, accumulator) {
+  for (let i = 0; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i]);
+  }
+  return accumulator;
+}
+
+function foldRight(array, callback, accumulator) {
+  for (let i = 0; i < array.length; i++) {
+    accumulator = callback(array[i], accumulator);
+  }
+  return accumulator;
+}
+
+function reduce(array, callback) {
+  if (array.length < 1) {
+    throw new Error("Array must have at least one element");
+  }
+  let accumulator = array[0];
+  for (let i = 1; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i]);
+  }
+  return accumulator;
+}
+function reduceRight(array, callback) {
+  if (array.length < 1) {
+    throw new Error("Array must have at least one element");
+  }
+  let accumulator = array[0];
+  for (let i = 1; i < array.length; i++) {
+    accumulator = callback(array[i], accumulator);
+  }
+  return accumulator;
+}
+const adder = (a, b) => a + b;
+console.log(fold([1, 2, 3], adder, 2));
+console.log(foldRight([1, 2, 3], adder, 2));
+console.log(reduce([1, 2, 3], adder));
+console.log(reduceRight([1, 2, 3], adder));
