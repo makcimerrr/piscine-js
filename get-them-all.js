b9b9ca2215e7bc19a0e11fc2ba660dc71f10229a
MaXDuB76 <dbmx76@gmail.com>
2023-12-07 10:23:28 +0100

    oijyug

--- get-them-all.js
+++ get-them-all.js
@@ -1,10 +1,13 @@
 export const getArchitects = () => {
   let architectsList = [];
+  let spanList = [];
   const allA = document.getElementsByTagName("a");
+  const allSpan = document.getElementsByTagName("span");
   for (let i = 0; i < allA.length; i++) {
     architectsList.push(allA[i]);
   }
-  return architectsList;
+  for (let i = 0; i < allSpan.length; i++) {
+    spanList.push(allSpan[i]);
+  }
+  return architectsList, spanList;
 };
-
-console.log(getArchitects(people));
