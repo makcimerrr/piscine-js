2f7ccb5dee3e3231c7268c9384e2069d4545a166
MaXDuB76 <dbmx76@gmail.com>
2023-12-07 11:41:07 +0100

    lkjkhg

--- get-them-all.js
+++ get-them-all.js
@@ -1,24 +1,39 @@
-/*export const getArchitects = () => {
-  const architectsArray = [];
-  const nonArchitectsArray = [];
-
-  for (const person of people) {
-    if (person.tag === "a" && person.active) {
-      const architectElement = document.createElement("a");
-      architectElement.textContent = person.id;
-      architectsArray.push(architectElement);
-    } else {
-      const nonArchitectElement = document.createElement(person.tag);
-      nonArchitectElement.textContent = person.id;
-      nonArchitectsArray.push(nonArchitectElement);
-    }
-  }
-
-  return [architectsArray, nonArchitectsArray];
-};*/
-
 export const getArchitects = () => {
   const architects = Array.from(document.getElementsByTagName("a"));
   const nonArchitects = Array.from(document.getElementsByTagName(":not(a)"));
   return [architects, nonArchitects];
+};
+
+export const getClassical = () => {
+  const classics = Array.from(document.getElementsByClassName("classical"));
+  const nonClassics = Array.from(
+    document.getElementsByClassName(":not(classical)")
+  );
+  return [classics, nonClassics];
+};
+export const getActive = () => {
+  const classicalArchitects = Array.from(
+    document.getElementsByClassName("classical")
+  );
+  const activeClassicalArchitects = classicalArchitects.filter((element) =>
+    element.classList.contains("active")
+  );
+  const nonActiveClassicalArchitects = classicalArchitects.filter(
+    (element) => !element.classList.contains("active")
+  );
+  return [activeClassicalArchitects, nonActiveClassicalArchitects];
+};
+export const getBonannoPisano = () => {
+  const result = document.getElementById("BonannoPisano");
+  const classicalArchitects = Array.from(
+    document.getElementsByClassName("classical")
+  );
+  const nonActiveClassicalArchitects = classicalArchitects.filter(
+    (element) => !element.classList.contains("active")
+  );
+
+  const response = nonActiveClassicalArchitects.filter((element) =>
+    element.classList.contains(":not(#BonannoPisano)")
+  );
+  return [result, response];
 };
