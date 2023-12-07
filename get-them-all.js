e3f2382dbae268c89b309a77b81eae7c1d2c0641
MaXDuB76 <dbmx76@gmail.com>
2023-12-07 10:40:13 +0100

    lkjhg

--- get-them-all.js
+++ get-them-all.js
@@ -1,13 +1,18 @@
 export const getArchitects = () => {
-  let architectsList = [];
-  let spanList = [];
-  const allA = document.getElementsByTagName("a");
-  const allSpan = document.getElementsByTagName("span");
-  for (let i = 0; i < allA.length; i++) {
-    architectsList.push(allA[i]);
+  const architectsArray = [];
+  const nonArchitectsArray = [];
+
+  for (const person of people) {
+    if (person.tag === "a" && person.active) {
+      const architectElement = document.createElement("a");
+      architectElement.textContent = person.id;
+      architectsArray.push(architectElement);
+    } else {
+      const nonArchitectElement = document.createElement(person.tag);
+      nonArchitectElement.textContent = person.id;
+      nonArchitectsArray.push(nonArchitectElement);
+    }
   }
-  for (let i = 0; i < allSpan.length; i++) {
-    spanList.push(allSpan[i]);
-  }
-  return architectsList, spanList;
+
+  return [architectsArray, nonArchitectsArray];
 };
