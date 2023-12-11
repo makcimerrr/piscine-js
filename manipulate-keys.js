b32cd49a2b4947c3c885253f631df36e2bd52c56
MaXDuB76 <dbmx76@gmail.com>
2023-12-11 14:07:46 +0100

    lmkjhj

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -20,12 +20,25 @@ function reduceKeys(obj, reducer, initialValue = "") {
 
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
-      acc = reducer(acc, key);
+      if (acc === "") {
+        acc += key;
+      } else {
+        acc = reducer(acc, key);
+      }
     }
   }
 
   return acc;
 }
+
+const cart = {
+  vinegar: 80,
+  sugar: 100,
+  oil: 50,
+  onion: 200,
+  garlic: 22,
+  paprika: 4,
+};
 
 const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
 
@@ -37,3 +50,7 @@ console.log(mapKeys(nutrients, (k) => `-${k}`));
 
 console.log(reduceKeys(nutrients, (acc, cr) => acc.concat(", ", cr)));
 // output: carbohydrates, protein, fat
+
+console.log(reduceKeys(cart, (acc, cr) => `${acc}${cr}:`, ":"));
+
+console.log(reduceKeys(cart, (acc, cr) => acc.concat(", ", cr)));
