a168c026413353008f4dfd71ba2edbd89af9aba8
MaXDuB76 <dbmx76@gmail.com>
2023-12-12 15:54:34 +0100

    ljkhb

--- deep-copy.js
+++ deep-copy.js
@@ -1,3 +1,30 @@
-function deepCopy(obj) {
-  return JSON.parse(JSON.stringify(obj));
+function deepCopy(input) {
+  var output;
+  switch (true) {
+    case Array.isArray(input):
+      output = [];
+      for (var i = 0; i < input.length; i++) {
+        output[i] = deepCopy(input[i]);
+      }
+      break;
+    case isDefenitelyAnObject(input):
+      output = {};
+      for (var key in input) {
+        output[key] = deepCopy(input[key]);
+      }
+      break;
+    default:
+      output = input;
+  }
+  return output;
+}
+
+function isDefenitelyAnObject(input) {
+  return (
+    typeof input === "object" &&
+    !(typeof input === "function") &&
+    !Array.isArray(input) &&
+    input !== null &&
+    !(input instanceof RegExp)
+  );
 }
