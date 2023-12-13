3630ab9818608f521da33fba2d62bd2dd7c35743
MaXDuB76 <dbmx76@gmail.com>
2023-12-13 10:41:38 +0100

    knjbh

--- debounce.js
+++ debounce.js
@@ -28,32 +28,18 @@ function debounce(func, wait = 0, options = {}) {
   };
 }
 
-function opDebounce(func, wait = 0, options = {}) {
+function opDebounce(func, delay, options = {}) {
   let timeout;
-  let isImmediate = false;
-
-  if (options.leading) {
-    isImmediate = true;
-  }
-
-  return function () {
-    const context = this;
-    const args = arguments;
-
-    const later = function () {
-      timeout = null;
-      if (!isImmediate) {
-        func.apply(context, args);
-      }
-    };
-
-    const callNow = isImmediate && !timeout;
-
-    clearTimeout(timeout);
-    if (callNow) {
-      func.apply(context, args);
-    } else {
-      timeout = setTimeout(later, wait);
+  return function (...args) {
+    if (options.leading && !timeout) {
+      func(...args);
     }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      if (!options.leading) {
+        func(...args);
+      }
+    }, delay);
   };
 }
