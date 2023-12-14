e1280175f863173b4d42413d8e583fcbf5bf208b
MaXDuB76 <dbmx76@gmail.com>
2023-12-14 15:07:51 +0100

    ùmljkhj

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,7 +1,27 @@
 function retry(count, callback) {
-  try {
-    return callback(count);
-  } catch (error) {
-    console.error(error);
-  }
+  return async function (...args) {
+    try {
+      return await callback(...args);
+    } catch (error) {
+      if (count > 0) {
+        return retry(count - 1, callback)(...args);
+      } else {
+        throw error;
+      }
+    }
+  };
 }
+
+const setup = {
+  r: Math.random().toString(36).slice(2),
+  failNTimes:
+    (n) =>
+    async (...v) =>
+      --n < 0 ? v : Promise.reject(Error(`x:${v}`)),
+  delayed:
+    (delay) =>
+    (...v) =>
+      new Promise((s) => setTimeout(s, delay, v)),
+};
+
+console.log(retry(0, setup.failNTimes(0))(setup.r));
