4fcae7318bd2fd513f7472477b8d271007a9524e
MaXDuB76 <dbmx76@gmail.com>
2023-12-14 11:34:08 +0100

    lkjh

---
+++ interpolation.js
@@ -0,0 +1,14 @@
+function interpolation(options = {}) {
+  const abscisse = (options.end - options.start) / options.step;
+  const ordonnee = options.duration / options.step;
+  let i = 0;
+  const timer = setInterval(() => {
+    if (i < options.step) {
+      callback([options.start, ordonnee * (i + 1)]);
+      options.start += abscisse;
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, options.duration / options.step);
+}
