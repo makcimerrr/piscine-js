f6244b751bb7653a9c85d4e2231efcf7fa3e442a
MaXDuB76 <dbmx76@gmail.com>
2023-12-15 08:48:49 +0100

    mlkjk

---
+++ get-json.js
@@ -0,0 +1,30 @@
+async function getJSON(path, params) {
+  try {
+    const url = new URL(path);
+    if (params) {
+      Object.keys(params).forEach((key) =>
+        url.searchParams.append(key, params[key])
+      );
+    }
+
+    const response = await fetch(url);
+
+    // Vérifier si la réponse est OK
+    if (!response.ok) {
+      throw new Error(`Erreur: ${response.statusText}`);
+    }
+
+    const json = await response.json();
+
+    //Contient data ou error ?
+    if (json.hasOwnProperty("data")) {
+      return json.data;
+    } else if (json.hasOwnProperty("error")) {
+      throw new Error(`Error: ${json.error}`);
+    } else {
+      throw new Error("Error");
+    }
+  } catch (error) {
+    throw error;
+  }
+}
