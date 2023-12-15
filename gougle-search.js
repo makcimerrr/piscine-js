611b3361c6c149eb650fd3c2849cfd95acbafc1e
MaXDuB76 <dbmx76@gmail.com>
2023-12-15 11:44:09 +0100

    mkljh

---
+++ gougle-search.js
@@ -0,0 +1,38 @@
+// fake `getJSON` function
+/*let getJSON = async (url) => url;*/
+
+function queryServers(serverName, q) {
+  const url1 = `/${serverName}?q=${q}`;
+  const url2 = `/${serverName}_backup?q=${q}`;
+
+  return Promise.race([
+    getJSON(url1),
+    getJSON(url2),
+    new Promise((_, reject) =>
+      setTimeout(() => reject(new Error("timeout")), 80)
+    ),
+  ]);
+}
+
+async function gougleSearch(query) {
+  try {
+    const [webResult, imageResult, videoResult] = await Promise.all([
+      queryServers("web", query),
+      queryServers("image", query),
+      queryServers("video", query),
+    ]);
+
+    return {
+      web: webResult,
+      image: imageResult,
+      video: videoResult,
+    };
+  } catch (error) {
+    return Error("timeout");
+  }
+}
+
+// Example usage
+gougleSearch("hello+world")
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
