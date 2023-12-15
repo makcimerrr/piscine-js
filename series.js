64c57b5eea85c2541cb230d4a2d7ea9d2d76d231
MaXDuB76 <dbmx76@gmail.com>
2023-12-15 15:03:04 +0100

    lkjh

--- series.js
+++ series.js
@@ -1,3 +1,13 @@
 async function series(entries) {
-  return Promise.all(entries.map((func) => func()));
+  const data = [];
+  for (const func of entries) {
+    await series.lock;
+    try {
+      const result = await func();
+      data.push(result);
+    } finally {
+      series.lock = Promise.resolve();
+    }
+  }
+  return data;
 }
