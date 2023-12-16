ee415cfc404f1580107e7ec4016bbe38cf214175
MaXDuB76 <dbmx76@gmail.com>
2023-12-16 10:54:01 +0100

    lmkj

--- race.js
+++ race.js
@@ -12,33 +12,33 @@ function race(promises) {
   });
 }
 
-function some(promises, count) {
-  return new Promise((resolve) => {
-    if (promises.length === 0 || count === 0) {
-      resolve(undefined);
-      return;
+async function some(promises, count) {
+  if (promises.length === 0 || count === 0) {
+    return [];
+  }
+
+  let resolvedCount = 0;
+  const resolvedValues = [];
+  for (const promise of promises) {
+    try {
+      const result = await Promise.resolve(promise);
+      resolvedValues.push(result);
+      resolvedCount++;
+
+      if (resolvedCount === count) {
+        return resolvedValues;
+      }
+    } catch (error) {
+      resolvedCount++;
+
+      if (resolvedCount === count) {
+        return resolvedValues;
+      }
     }
+  }
 
-    let resolvedCount = 0;
-    const resolvedValues = [];
-
-    promises.forEach((promise) => {
-      Promise.resolve(promise)
-        .then((result) => {
-          resolvedValues.push(result);
-          resolvedCount++;
-
-          if (resolvedCount === count) {
-            resolve(resolvedValues);
-          }
-        })
-        .catch(() => {
-          resolvedCount++;
-
-          if (resolvedCount === count) {
-            resolve(resolvedValues);
-          }
-        });
-    });
-  });
+  return resolvedValues;
 }
+
+// Example usage:
+console.log(await some([], 10));
